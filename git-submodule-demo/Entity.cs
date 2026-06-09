969897f60d954dc08a20de730ae56f15fef8422d
Matthew Kaufman <mkaufman@vfccu.org>
2026-06-09 16:55:56 -0400

    Changing to UTC Now

--- git-submodule-demo/Entity.cs
+++ git-submodule-demo/Entity.cs
@@ -3,7 +3,7 @@
 public record Entity
 {
     public Guid Id {get; set;}
-    public DateTimeOffset DateCreated {get; set;} = DateTimeOffset.Now;
+    public DateTimeOffset DateCreated {get; set;} = DateTimeOffset.UtcNow;
     public DateTimeOffset? DateModified {get; set;}
     public string ModifiedBy {get; set;} = "Git Submodule";
 } 
